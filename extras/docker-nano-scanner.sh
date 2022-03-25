3502816a76b2c2a0b26bf195cf6ee41b8504d48c
Nana Insaidoo <insaidoo.nana@yahoo.it>
2022-03-25 15:31:18 +0000

    Now processing scan outcome
    - Changes were also applied to allow the script to exit with codes based on whether
      the the analysis scores meet the minimum thresholds or not (in line with what the Meterian Cli does)

--- extras/docker-nano-scanner.sh
+++ extras/docker-nano-scanner.sh
@@ -63,7 +63,7 @@ function uploadDependenciesToBuild() {
         echo "Done."
     else
         echo "Unexpected error encountered uploading dependencies."
-        exit 1
+        exit -1
     fi
 }
 
@@ -80,7 +80,7 @@ function startBuild() {
         echo "The build has started."
     else
         echo "Unexpected error encountered, the build could not be started."
-        exit 1
+        exit -1
     fi
 }
 
@@ -95,6 +95,7 @@ function getBuildStatus() {
 function jsonAttrToConsoleLine() {
     attr="$1"
     line="$(echo "$attr" | tr -d '"')"
+    line="$(echo "$line" | tr -d ',')"
     line="${line//:/: }"
 
     echo "$line"
@@ -122,27 +123,121 @@ function pollBuildStatus() {
 
     if [[ $retries -eq $max_retries && -z "$status" ]]; then
         echo "Unexpected error, build status polling timed out"
-        exit 1
+        exit -1
     fi
 }
 
 function printFinalResults() {
+    pid="${1:-}"
+    security="${2:-}"
+    stability="${3:-}"
+    licensing="${4:-}"
+
+    echo "Final results:"
+    echo "- security: $security"
+    echo "- stability $stability"
+    if [[ -n "$licensing" ]]; then
+        echo "- licensing $licensing"
+    fi
+    echo
+
+    echo "Full report available at:"
+    echo "$METERIAN_API_BASE_URL/projects/?pid=$pid&mode=eli"
+}
+
+function processFinalResults() {
     response="$1"
-    pid="$(echo $response | grep -oE '\s*"uuid"\s*:\s*"[a-z0-9-]*"')"
+    project_branch="$2"
+
+    accountUuid="$(echo "${response:-}" | grep -oE '\s*"account_uuid"\s*:\s*"[a-z0-9-]*"')"
+    accountUuid="$(jsonAttrToConsoleLine "$accountUuid")"
+    accountUuid=${accountUuid:14}
+
+    if [[ -z "$accountUuid" ]]; then
+        echo "Unable to process final results, account ID was found unset"
+        echo
+        exit -1
+    fi
+
+    # registering outcome
+
+    config_response="$(getConfiguration "$accountUuid")"
+    min_security="$(echo "${config_response:-}" | grep -oE '\s*"security"\s*:\s*[0-9]*,')"
+    min_security="$(jsonAttrToConsoleLine "$min_security")"
+    min_security=${min_security:10}
+    min_security=${min_security:-90}
+
+    min_stability="$(echo "${config_response:-}" | grep -oE '\s*"stability"\s*:\s*[0-9]*,')"
+    min_stability="$(jsonAttrToConsoleLine "$min_stability")"
+    min_stability=${min_stability:11}
+    min_stability=${min_stability:-80}
+
+    min_licensing="$(echo "${config_response:-}" | grep -oE '\s*"licensing"\s*:\s*[0-9]*,')"
+    min_licensing="$(jsonAttrToConsoleLine "$min_licensing")"
+    min_licensing=${min_licensing:11}
+    min_licensing=${min_licensing:-95}
+
+    pid="$(echo "${response:-}" | grep -oE '\s*"uuid"\s*:\s*"[a-z0-9-]*"')"
     pid="$(jsonAttrToConsoleLine "$pid")"
     pid="${pid//uuid: /}"
 
-    security="$(echo $response | grep -oE '\s*"security"\s*:\s*"[0-9]*"')"
-    stability="$(echo $response | grep -oE '\s*"stability"\s*:\s*"[0-9]*"')"
-    licensing="$(echo $response | grep -oE '\s*"licensing"\s*:\s*"[0-9]*"')"
+    security_score="$(echo "${response:-}" | grep -oE '\s*"security"\s*:\s*"[0-9]*"')"
+    security_score="$(jsonAttrToConsoleLine "$security_score")"
+    security_score=${security_score:10}
 
-    echo "Final results:"
-    echo "- $(jsonAttrToConsoleLine "$security")"
-    echo "- $(jsonAttrToConsoleLine "$stability")"
-    echo "- $(jsonAttrToConsoleLine "$licensing")"
+    stability_score="$(echo "${response:-}" | grep -oE '\s*"stability"\s*:\s*"[0-9]*"')"
+    stability_score="$(jsonAttrToConsoleLine "$stability_score")"
+    stability_score=${stability_score:11}
+
+    licensing_score="$(echo "${response:-}" | grep -oE '\s*"licensing"\s*:\s*"[0-9]*"')"
+    licensing_score="$(jsonAttrToConsoleLine "$licensing_score")"
+    licensing_score=${licensing_score:11}
+
+    if [[ -z "$pid" || -z "$security_score" || -z "$stability_score" ]]; then
+        echo "Unable to process final results, final results data was found unset"
+        echo
+        exit -1
+    fi
+
+    outcome=0
+    if [[ $min_security -gt $security_score ]];then
+        outcome=$(( $outcome + 1 ))
+    fi
+
+    if [[ $min_stability -gt $stability_score ]];then
+        outcome=$(( $outcome + 2 ))
+    fi
+
+    if [[ -n "$licensing_score" && -n "$min_licensing" ]];then
+        if [[ $min_licensing -gt $licensing_score ]]; then
+            outcome=$(( $outcome + 4 ))
+        fi
+    fi
+
+    if [[ $outcome -eq 0 ]];then
+        recordOutcome "$pid" "$project_branch" "PASS"
+    else
+        recordOutcome "$pid" "$project_branch" "FAIL"
+    fi
+
+    # print final results
+    printFinalResults "$pid" "$security_score" "$stability_score" "$licensing_score"
     echo
-    echo "Full report available at:"
-    echo "$METERIAN_API_BASE_URL/projects/?pid=$pid&mode=eli"
+
+    exit $outcome
+}
+
+function recordOutcome() {
+    curl -sS -X POST "$METERIAN_API_BASE_URL/api/v1/reports/${1}/outcome?branch=${2}&outcome=${3}" \
+    -H  "accept: application/json" -H  "Authorization: Token $METERIAN_API_TOKEN" > /dev/null 2>&1
+}
+
+function getConfiguration() {
+    response="$(curl -sS -X GET "$METERIAN_API_BASE_URL/api/v1/accounts/${1}/configuration" \
+    -H "accept: */*" \
+    -H "Authorization: Token $METERIAN_API_TOKEN" 2> /dev/null )"
+
+    echo "$response"
 }
 
 function getRdependencyList() {
@@ -205,7 +300,7 @@ echo
 build_id="$(createBuild "$CREATE_BUILD_DATA_JSON")" 
 if [[ -z "$build_id" ]]; then
     echo "Unexpected error occurred, unable to create build."
-    exit 1
+    exit -1
 else
     echo "Build successfully created: ${build_id}"
     echo
@@ -231,5 +326,5 @@ echo
 pollBuildStatus "$build_id"
 
 echo
-printFinalResults "$(getBuildStatus "$build_id")"
+processFinalResults "$(getBuildStatus "$build_id")" $IMAGE_TAG
 echo
